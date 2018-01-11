36c03cb65560e17ffc305cb0c277ae94ad5f0065
Erica Giordo <ericagiordo@OVO3575MB.local>
2018-01-11 11:56:30 +0000

    Replaced |@| with .mapN

--- src/main/scala/com/ovoenergy/comms/templates/model/template/processed/email/EmailTemplate.scala
+++ src/main/scala/com/ovoenergy/comms/templates/model/template/processed/email/EmailTemplate.scala
@@ -1,7 +1,7 @@
 package com.ovoenergy.comms.templates.model.template.processed.email
 
 import cats.instances.option._
-import cats.syntax.cartesian._
+import cats.syntax.apply._
 import cats.syntax.traverse._
 import cats.{Applicative, Apply, Id}
 import com.ovoenergy.comms.templates._
@@ -17,14 +17,14 @@ case class EmailTemplate[M[_]: Applicative](
 ) {
 
   def aggregate: M[EmailTemplate[Id]] = {
-    (subject |@| htmlBody |@| textBody.sequence |@| sender.sequence) map {
+    (subject, htmlBody, textBody.sequence, sender.sequence).mapN {
       EmailTemplate[Id](_, _, _, _)
     }
   }
 
   def requiredData: M[ErrorsOr[RequiredTemplateData.obj]] = {
     import cats.instances.list._
-    (subject |@| htmlBody |@| textBody.sequence) map {
+    (subject, htmlBody, textBody.sequence).mapN {
       case (s, h, t) =>
         val templates: List[HandlebarsTemplate]           = List(Some(s), Some(h), t).flatten
         val requiredDatas: List[RequiredTemplateData.obj] = templates.map(_.requiredData)
